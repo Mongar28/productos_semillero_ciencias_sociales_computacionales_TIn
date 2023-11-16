bc60ceacbd96124bc89a06dbf7deab4ca281f4e3
Mongar28 <cristian.montoya.g@gmail.com>
2023-11-15 22:50:28 -0500

    actualizacion de whisper

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 pip install -r /opt/requirements.txt
+pip install pip install --upgrade transformers accelerate datasets[audio]
 
 # ls -l /app
 streamlit run /app/transcripcion.py --server.port=8080
