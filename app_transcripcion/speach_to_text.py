acf9fd929b75c496b85acd27e298ede85ed89776
Mongar28 <cristian.montoya.g@gmail.com>
2023-12-09 02:00:38 -0500

    actualización de los modelos open source de whisper

--- app_transcripcion/speach_to_text.py
+++ app_transcripcion/speach_to_text.py
@@ -12,7 +12,9 @@ import datetime
 import time
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
+from transformers import pipeline, AutoModelForCausalLM, AutoModelForSpeechSeq2Seq, AutoProcessor
+
+
 
 
 
@@ -108,7 +110,7 @@ def procesamiento_audio2(audio):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    model_id = "openai/whisper-medium"
+    model_id = "openai/whisper-base"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
@@ -152,7 +154,7 @@ def whisper3():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    model_id = "openai/whisper-medium"
+    model_id = "openai/whisper-base"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
@@ -218,3 +220,46 @@ def importar_audio_file2()->str:
 
     
     return nombre_archivo
+
+
+
+
+
+@st.cache_resource
+def whisper4():
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+    assistant_model_id = "distil-whisper/distil-large-v2"
+
+    assistant_model = AutoModelForCausalLM.from_pretrained(
+        assistant_model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    assistant_model.to(device)
+
+    model_id = "openai/whisper-large-v2"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        generate_kwargs={"assistant_model": assistant_model},
+        torch_dtype=torch_dtype,
+        device=device,
+    )
+
+    
+    return pipe
--- transcripcion.py
+++ transcripcion.py
@@ -9,7 +9,8 @@ from donaciones.donaciones_sc2 import donaciones
 from app_transcripcion.speach_to_text import (mensaje_intruncciones,
                                               importar_audio_file2,
                                               procesamiento_audio3,
-                                              whisper3)
+                                              whisper3,
+                                              whisper4)
 
 
 from datetime import datetime
@@ -17,10 +18,9 @@ import datetime
 import time
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
 
-pipe = whisper3()
-result = None
+
+
 
 
 # Importamos y corremos la funcion encabezado del modulo mensaje principal. 
@@ -28,6 +28,9 @@ encabezado()
 
 # importamos y corremos la funcion mensaje_intruncciones  del modulo speach_to_text que contiene el mensaje y las instrucciones de la app
 mensaje_intruncciones()
+with st.spinner('Cargando el modelo para la transcripción...', ):
+    pipe = whisper4()
+    result = None
 
 formulario_enviado = False
 
@@ -44,28 +47,32 @@ if formulario_enviado == True:
     
     historial_usuario(correo, 'transcriptor') 
     # importamos y corremos la funcion procesamiento_audio del modulo speach_to_text que contiene los scripts para procesar el audio con Whisper
-    result = pipe(ruta_archivo, return_timestamps=True, generate_kwargs={"language": "spanish"})
-    list_transcripciones = procesamiento_audio3(ruta_archivo, nombre_archivo, result)
-    transcripcion_doc(list_transcripciones)
-    donaciones()
     
-    archivo_json = 'historial_uso_app.json'
-    if os.path.exists(archivo_json):
-        with open(archivo_json, "r") as json_file:
-            datos_uso = json.load(json_file)
-    else:
-        datos_uso = []  
+    with st.spinner('Transcribien... El proceso puede tardar.', ):
+        result = pipe(ruta_archivo, return_timestamps=True, generate_kwargs={"language": "spanish"})
         
-    from datetime import datetime
-    fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    datos = {
-        'nombre_usuario': nombre,
-        'correo' : correo,
-        'fecha': fecha_actual,
-        'aplicacion_usada': 'transcriptor'
-    }
+        
+        list_transcripciones = procesamiento_audio3(ruta_archivo, nombre_archivo, result)
+        transcripcion_doc(list_transcripciones)
+        donaciones()
+        
+        archivo_json = 'historial_uso_app.json'
+        if os.path.exists(archivo_json):
+            with open(archivo_json, "r") as json_file:
+                datos_uso = json.load(json_file)
+        else:
+            datos_uso = []  
+            
+        from datetime import datetime
+        fecha_actual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        datos = {
+            'nombre_usuario': nombre,
+            'correo' : correo,
+            'fecha': fecha_actual,
+            'aplicacion_usada': 'transcriptor'
+        }
 
-    datos_uso.append(datos)
-    with open(archivo_json, "w", encoding="utf-8") as json_file:
-        json.dump(datos_uso, json_file, indent=4, ensure_ascii=False)  
+        datos_uso.append(datos)
+        with open(archivo_json, "w", encoding="utf-8") as json_file:
+            json.dump(datos_uso, json_file, indent=4, ensure_ascii=False)  
